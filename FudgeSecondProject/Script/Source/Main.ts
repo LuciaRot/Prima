e935d013c24c15cbafd2236e1a6730e34d242d92
LuciaRot <sweetlona3@gmail.com>
2023-04-05 14:40:34 +0200

    Added character

--- FudgeSecondProject/Script/Source/Main.ts
+++ FudgeSecondProject/Script/Source/Main.ts
@@ -3,18 +3,23 @@ namespace Script {
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
+  let woman: ƒ.Node; 
+
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
+  
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
-
+    woman = viewport.getBranch().getChildrenByName("Woman")[0];
+    woman.mtxLocal.translateX(1);
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
+    woman.mtxLocal.translateX(0.01);
   }
 }
